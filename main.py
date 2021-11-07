a2a5c07932e0881b45efd51fb9e83c9d9b818ea0
kosynka <sayat.kaldarbekov.00@gmail.com>
2021-11-07 23:32:46 +0600

    создал окно и фигуры на правом краю

--- main.py
+++ main.py
@@ -1,61 +1,68 @@
 import pygame
-import random
+import time
+import sys
 
-WIDTH = 800
-HEIGHT = 650
-FPS = 30
+pygame.font.init()
 
-# Задаем цвета
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
+white = (255, 255, 255)
+grey = (128, 128, 128)
+yellow = (204, 204, 0)
+blue = (50, 255, 255)
+black = (0, 0, 0)
+
+(width, height) = (900, 600)
+screen = pygame.display.set_mode((width, height))
+
+pygame.display.set_caption("Quadro table game")
+screen.fill(white)
+pygame.display.flip()
+
+myfont = pygame.font.SysFont('Times New Roman', 20)
+
+board = [['  ' for i in range(8)] for i in range(8)]
+
+class Piece:
+    def __init__(self, name, image, pos):
+        self.name = name
+        self.image = image
+        self.pos = pos
 
 
-class Player(pygame.sprite.Sprite):
-    def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((50, 50))
-        self.image.fill(GREEN)
-        self.rect = self.image.get_rect()
-        self.rect.center = (WIDTH / 2, HEIGHT / 2)
-        
-        
-    def update(self):
-        self.rect.x += 5
-        if self.rect.left > WIDTH:
-            self.rect.right = 0
+# Figures
+chwh = Piece('chwh', 'chwh.png', (700, 30))
+chwn = Piece('chwn', 'chwn.png', (720, 30))
+chbh = Piece('chbh', 'chbh.png', (740, 30))
+chbn = Piece('chbn', 'chbn.png', (760, 30))
 
+clwh = Piece('clwh', 'clwh.png', (700, 50))
+clwn = Piece('clwn', 'clwn.png', (720, 50))
+clbh = Piece('clbh', 'clbh.png', (740, 50))
+clbn = Piece('clbn', 'clbn.png', (760, 50))
 
-# Создаем игру и окно
-pygame.init()
-pygame.mixer.init()
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Quadro game")
-clock = pygame.time.Clock()
-all_sprites = pygame.sprite.Group()
-player = Player()
-all_sprites.add(player)
+shwh = Piece('shwh', 'shwh.png', (700, 70))
+shwn = Piece('shwn', 'shwn.png', (720, 70))
+shbh = Piece('shbh', 'shbh.png', (740, 70))
+shbn = Piece('shbn', 'shbn.png', (760, 70))
 
-# Цикл игры
+slwh = Piece('slwh', 'slwh.png', (700, 90))
+slwn = Piece('slwn', 'slwn.png', (720, 90))
+slbh = Piece('slbh', 'slbh.png', (740, 90))
+slbn = Piece('slbn', 'slbn.png', (760, 90))
+
+global gameObjectList
+gameObjectList = [chwh, chwn, chbh, chbn, clwh, clwn, clbh, clbn, shwh, shwn, shbh, shbn, slwh, slwn, slbh, slbn]
+
 running = True
 while running:
-    # Держим цикл на правильной скорости
-    clock.tick(FPS)
-    # Ввод процесса (события)
     for event in pygame.event.get():
-        # check for closing window
         if event.type == pygame.QUIT:
             running = False
-
-    # Обновление
-    all_sprites.update()
     
-    # Рендеринг
-    screen.fill(BLACK)
-    all_sprites.draw(screen)
-    # После отрисовки всего, переворачиваем экран
-    pygame.display.flip()
+    label = myfont.render("Привет. Чтобы начать играть перетасивайте фигуры мышкой по очереди", 1, black)
+    screen.blit(label, (100, 10))
 
-pygame.quit()
+    for indx,_ in enumerate(gameObjectList):
+        curObj = gameObjectList[indx]
+        screen.blit(curObj.image, curObj.pos)
+
+    pygame.display.update()
