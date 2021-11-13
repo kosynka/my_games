6798e97ea29558873b911c5667339892807bfb9c
kosynka <sayat.kaldarbekov.00@gmail.com>
2021-11-13 19:08:00 +0600

    MineSweeper: inserted random mines in list

--- saper.py
+++ saper.py
@@ -1,25 +1,35 @@
-
+from random import shuffle
 import tkinter as tk
 
 class MyButton(tk.Button):
 
+    def __init__(self, master, x, y, number, *args, **kwargs):
+        super(MyButton, self).__init__(master,  width=4, font='Calibri 15 bold', *args, **kwargs)
+        self.x = x
+        self.y = y
+        self.number = number
+        self.is_mine = False
+
     def __repr__(self):
-        return 'mb'
+        return f'MyButton{self.x}{self.y} {self.number} {self.is_mine}'
 
 
 
 class MineSweeper:
 
     window = tk.Tk()
-    ROWS = COLUMNS = 8
+    ROWS = COLUMNS = 5
+    MINES = 5
 
     def __init__(self):
         self.buttons = []
+        count = 1
         for i in range(MineSweeper.ROWS):
             temp = []
             for j in range(MineSweeper.COLUMNS):
-                button = MyButton(MineSweeper.window, width=4, font='Calibri 15 bold')
+                button = MyButton(MineSweeper.window, x=i, y=j, number=count)
                 temp.append(button)
+                count += 1
             self.buttons.append(temp)
     
     def create_widgets(self):
@@ -31,10 +41,26 @@ class MineSweeper:
     def print_buttons(self):
         for row_button in self.buttons:
             print(row_button)
+    
+    @staticmethod
+    def get_mines_places():
+        indexees = list(range(1, MineSweeper.COLUMNS * MineSweeper.ROWS + 1))
+        shuffle(indexees)
+        return indexees[:MineSweeper.MINES]
 
+    def insert_mines(self):
+        index_mines = self.get_mines_places()
+        print(index_mines)
+        for row_button in self.buttons:
+            for button in row_button:
+                if button.number in index_mines:
+                    button.is_mine = True
+    
     def start_game(self):
         self.create_widgets()
+        self.insert_mines()
         self.print_buttons()
+
         MineSweeper.window.mainloop()
 
 
